07d5bdbb058adbe83280c88539f01d7a9c2580a1
talha <mtalha3597@gmail.com>
2021-08-22 12:46:37 +0500

    prepare for deployment

--- server/model/db.js
+++ server/model/db.js
@@ -1,5 +1,5 @@
 const mongoose =require('mongoose');
-DB_URI="mongodb+srv://amir:aamir123@cluster0.0fib0.mongodb.net/fazalnoor?retryWrites=true&w=majority"
+DB_URI=process.env.MONGODB
 //mongodb://127.0.0.1:27017/school
 
  
